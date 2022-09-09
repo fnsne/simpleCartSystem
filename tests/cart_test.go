d03f5307052772d1a0ca08c260c98aa875744cc9
fnsne <watashiwafnsne@gmail.com>
2022-09-09 14:59:54 +0800

    when there is no cart waiting for checkout,will generate one through GetCart

--- tests/cart_test.go
+++ tests/cart_test.go
@@ -75,6 +75,38 @@ func (suite *CartTests) Test_GetCart() {
 	})
 
 }
+func (suite *CartTests) Test_GetCart_whenNoCartWaitingForCheckout() {
+	GivenProducts([]model.Product{
+		{
+			Model:     gorm.Model{ID: 1},
+			Name:      "product 1",
+			Price:     decimal.NewFromInt(10),
+			Inventory: 3,
+		},
+		{
+			Model:     gorm.Model{ID: 2},
+			Name:      "product 2",
+			Price:     decimal.NewFromInt(20),
+			Inventory: 5,
+		},
+	})
+	GivenCart(model.Cart{
+		UserID: 1,
+		Products: []model.CartProduct{
+			{ProductID: 1, Quantity: 1},
+			{ProductID: 2, Quantity: 3},
+		},
+		Amount:     decimal.NewFromInt(70),
+		IsCheckout: true,
+	})
+	suite.currentCartShouldBe(model.Cart{
+		UserID:   1,
+		Products: []model.CartProduct{},
+		Amount:   decimal.NewFromInt(0),
+	})
+
+}
+
 func (suite *CartTests) Test_GetCart_withoutExistCart() {
 	GivenProducts([]model.Product{
 		{
